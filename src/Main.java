740fff55a88d3e2e66e1df6aab540778f07caab7
Irem Ozkan <irem5455367714@gmail.com>
2026-05-19 18:31:59 +0300

    online kurs sepeti sistemi guncellendi

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,24 @@ public class Main {
 
         String indirimTipi = "ogrenci";
 
-        double fiyat = 1000;
-        double sonuc = fiyat;
+        double javaKursu = 1200;
+        double pythonKursu = 1000;
+        double webKursu = 1500;
+
+        double toplam = javaKursu + pythonKursu + webKursu;
+        double sonuc = toplam;
 
         if(indirimTipi.equals("ogrenci")) {
-            sonuc = fiyat * 0.8;
+            sonuc = toplam * 0.8;
         }
-        else if(indirimTipi.equals("vip")) {
-            sonuc = fiyat * 0.7;
+        else if(indirimTipi.equals("abonman")) {
+            sonuc = toplam * 0.75;
         }
-        else if(indirimTipi.equals("bayram")) {
-            sonuc = fiyat * 0.5;
+        else if(indirimTipi.equals("kupon")) {
+            sonuc = toplam - 500;
         }
 
-        System.out.println("Toplam fiyat: " + sonuc);
+        System.out.println("Sepet toplami: " + toplam);
+        System.out.println("Indirimli toplam: " + sonuc);
     }
 }
