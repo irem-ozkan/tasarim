5ac68d55484af11d0d4ef79701d3d2569fc1821e
Irem Ozkan <irem5455367714@gmail.com>
2026-05-20 21:35:54 +0300

    JavaKursu.java dosyasina bilgiVer ve fiyatVer metodlari eklendi

---
+++ src/JavaKursu.java
@@ -0,0 +1,12 @@
+public class JavaKursu implements Kurs {
+
+    public String bilgiVer() {
+
+        return "Java Kursu";
+    }
+
+    public double fiyatVer() {
+
+        return 1200;
+    }
+}
