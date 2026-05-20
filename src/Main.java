061d6ab43c76c0efbb61dccb72df21afa85b9f2a
Irem Ozkan <irem5455367714@gmail.com>
2026-05-20 21:05:44 +0300

    Main.java dosyasina kurs fiyat degiskenleri eklendi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String indirimTipi = "ogrenci";
+
+        double javaKursu = 1200;
+        double pythonKursu = 1000;
+        double webKursu = 1500;
     }
 }
